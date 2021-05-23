459e25dba10f95d246d3a0d004341e842cefc689
Tal Shvartzberg <82408347+talshva@users.noreply.github.com>
2021-05-23 19:59:39 +0300

    Update bank.js

--- Encoding/js/services/bank.js
+++ Encoding/js/services/bank.js
@@ -33,6 +33,10 @@ for (var i = 97; i <= 122; i++) {
     lower_case.push(String.fromCharCode(i));
 }
 
+var b64_index_table = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']+
+['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']+
+['0','1','2','3','4','5','6','7','8','9','+','/'];
+
 function checkInOther(letter) {
     return !other.includes(letter);
 }
@@ -52,3 +56,7 @@ function checkInUpper(letter) {
 function checkInLower(letter) {
     return !lower_case.includes(letter);
 }
+
+function checkInB64(letter) {
+    return b64_index_table.includes(letter);
+}    
